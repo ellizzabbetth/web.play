9f1992e20d3af152527bbb2870aa960f754284ab
ellizzabbetth <z.z.bradley@gmail.com>
2014-01-26 07:40:29 -0500

    last commit

--- web.play/src/apps101/web/play/RoundballActivity.java
+++ web.play/src/apps101/web/play/RoundballActivity.java
@@ -13,7 +13,7 @@ public class RoundballActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 myWebView =(WebView) findViewById(R.id.webView1);	
+		myWebView =(WebView) findViewById(R.id.webView1);	
 		myWebView.getSettings().setDomStorageEnabled(true); 
 		myWebView.getSettings().setJavaScriptEnabled(true);
 		myWebView.loadUrl("file:///android_asset/roundball/roundball.html");
